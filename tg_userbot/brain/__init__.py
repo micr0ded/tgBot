1be9df4d8330b08b99571ba187052b68e4534094
Micr0Ded <mihail.belousow@gmail.com>
2022-12-16 23:53:13 +0400

    kek 2

--- tg_userbot/brain/__init__.py
+++ tg_userbot/brain/__init__.py
@@ -1,4 +1,4 @@
-from tg_userbot.brain.brain import Brain
-from tg_userbot.brain.memory import auth_data
+from brain.brain import Brain
+from brain.auth_data import auth_data
 
 __all__ = ["auth_data", "Brain"]
